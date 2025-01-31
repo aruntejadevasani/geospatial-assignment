605c4d56a2ccf9b2486918ba547535a2676a5d4f
Arun Teja <aruntejadevasani@gmail.com>
2025-01-31 23:46:52 +0530

    Removed unwanted code !

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import MapComponent from "./MapComponent";
-// import MapComponent2 from "./MapComponent2";
 
 function App() {
   return (
     <>
       <div className="App">Welcome to ArcGIS Map!</div>
-      {/* <div style={{ height: "80vh", width: "80vw" }}> */}
       <MapComponent />
-      {/* </div> */}
     </>
   );
 }
